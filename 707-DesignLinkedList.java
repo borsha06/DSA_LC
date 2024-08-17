4ccf1b43b0c4fbbe7592afe9f1c651d1ca487286
borsha06 <tasfia.sharmin1994@gmail.com>
2024-08-17 23:29:00 +0200

    added 701-LinkedList-Partial file

---
+++ 707-DesignLinkedList.java
@@ -0,0 +1,42 @@
+class MyLinkedList {
+
+    Node head;
+
+    class Node {
+
+        int x;
+        Node next;
+
+        public Node(int data) {
+            this.x = data;
+            this.next = null;
+        }
+
+    }
+
+    public MyLinkedList() {
+        this.head = null;
+
+    }
+
+    public int get(int index) {
+
+    }
+
+    public void addAtHead(int val) {
+
+    }
+
+    public void addAtTail(int val) {
+
+    }
+
+    public void addAtIndex(int index, int val) {
+
+    }
+
+    public void deleteAtIndex(int index) {
+
+    }
+}
+
